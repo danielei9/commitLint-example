eb67aa2550c65661f7114ae7c37b013b6d106480
danielei9 <dbs99@hotmail.com>
2023-11-09 10:28:55 +0100

    feat(node): change names

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-const { parseConventionalCommit,
-    execCommand,
+const {
+    execCommand,parseConventionalCommit,
     bumpVersion,
-    transformCommitRawToObject,
-    getBranch,
     updatePythonVersionFile, updateNodeVersionFile,
     readPythonVersion,
     findVersionBump
@@ -13,57 +11,32 @@ const nodePathVersionFile = process.env.NODE_PATH_VERSION_FILE;
 const pythonPathVersionFile = process.env.PYTHON_PATH_VERSION_FILE;
 
 async function main() {
-    // const commit = getCommit(process.argv[2])
-    const commit = "fix(lint): lint"
+    // Parse commits from last version (in case they come from git log)
+    const rawCommits = await execCommand(`git show --oneline --format=%s HEAD`);
+    const commitTitle = rawCommits.split("\n\ndiff --git")[0]
+    const commitObject = parseConventionalCommit(commitTitle)
     let updatedNodeVersion, updatedPythonVersion;
-
-    for (const commit of commits) {
-        // parse commit
-        const commit_parsed = transformCommitRawToObject(commit.children[0].children)
-        // find version Bump
-        let versionBump = findVersionBump(commit_parsed)
-
-        // Specify Scope
-        if (commit_parsed.scope == 'python') {
-            let currentPythonVersion = updatedPythonVersion || readPythonVersion(pythonPathVersionFile);
-            updatedPythonVersion = bumpVersion(currentPythonVersion, versionBump);
-        }
-        else if (commit_parsed.scope == 'node') {
-            let currentNodeVersion = updatedNodeVersion || require(nodePathVersionFile).version;
-            updatedNodeVersion = bumpVersion(currentNodeVersion, versionBump);
-        }
-        else {
-            // console.log("Can't specify scope.")
-        }
+    // parse commit
+    let versionBump = findVersionBump(commitObject)
+    // Specify Scope
+    if (commitObject.scope == 'python') {
+        let currentPythonVersion = updatedPythonVersion || readPythonVersion(pythonPathVersionFile);
+        updatedPythonVersion = bumpVersion(currentPythonVersion, versionBump);
+        console.log(versionBump)
+    }
+    else if (commitObject.scope == 'node') {
+        let currentNodeVersion = updatedNodeVersion || require(nodePathVersionFile).version;
+        updatedNodeVersion = bumpVersion(currentNodeVersion, versionBump);
+        console.log(versionBump)
+    }
+    else {
+        console.log("Can't specify scope.")
     }
     // Update Files
     updatePythonVersionFile(updatedPythonVersion, pythonPathVersionFile)
     updateNodeVersionFile(updatedNodeVersion, nodePathVersionFile)
 }
-
 main().catch(error => {
     console.error(error);
     process.exit(1);
 });
-const axios = require('axios');
-
-const getCommit = async (token) => {
-    
-    const organization = 'danielei9';
-    const repo = 'commitLint-example'
-    const commitSha = '16fbe8ec40733175fa5b8f94852cac4e596c072b'
-
-    const apiUrl = `https://api.github.com/repos/${organization}/${repo}/commits/${commitSha}`;
-    const config = {
-        headers: {
-            'Authorization': `token ${token}`,
-        }
-    };
-    const commit = await axios.get(apiUrl, config)
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    return parseConventionalCommit(commit.data.commit.message)
-}
-
-console.log(getCommit())
